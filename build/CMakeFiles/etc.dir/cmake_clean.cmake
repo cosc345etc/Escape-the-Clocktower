c8c0081496cc0398f3cbceb681ff355403b71f39
ahudson20 <anaru.hudson@hotmail.com>
2019-05-26 13:34:26 +1200

    makefiles

---
+++ build/CMakeFiles/etc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/etc.dir/main.c.o"
+  "etc.pdb"
+  "etc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/etc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
